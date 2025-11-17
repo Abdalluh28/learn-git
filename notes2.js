d8c3b08bb795f10b95de8f7dbd38fd9d390a8bc7
Abdalluh28 <abdok7496@gmail.com>
2025-11-17 20:36:44 +0200

    test for rebase branch testTwo

--- notes2.js
+++ notes2.js
@@ -14,6 +14,14 @@ git branch
 to rename a branch:
 git branch -m newbranchname
 
+to merge a branch into the current branch:
+git merge branchname
+we must be on the branch we want to merge into
+
+to delete a branch:
+git branch -d branchname
 
 
+to move commit(s) from one branch to another:
+git rebase branchname
 */
