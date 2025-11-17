9ace1e7f7cef1614331362f600ac909c17d3e086
Abdalluh28 <abdok7496@gmail.com>
2025-11-17 19:52:33 +0200

    commit to test branch

---
+++ notes2.js
@@ -0,0 +1,19 @@
+/*
+to make a new branch:
+git branch branchname
+git checkout -b branchname  (creates and switches to the new branch)
+git switch -c branchname  (creates and switches to the new branch)
+
+to switch between branches:
+git checkout branchname
+git switch branchname
+
+to see all branches:
+git branch
+
+to rename a branch:
+git branch -m newbranchname
+
+
+
+*/
